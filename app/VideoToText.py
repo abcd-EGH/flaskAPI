e76f74731d09d794195ce1ebd3b7a55e7d99f836
abcd-EGH <wlghks7790@gmail.com>
2023-12-28 21:41:15 +0900

    [delete] VideoToText 사용 예시 부분 삭제

--- app/VideoToText.py
+++ app/VideoToText.py
@@ -24,10 +24,3 @@ def video_to_text(video_file):
             return "음성 인식 실패: 음성을 이해할 수 없습니다."
         except sr.RequestError as e:
             return f"음성 인식 실패: {e}"
-    
-
-# 사용 예시
-# 비디오 이름이 한글일 시 호환되지 않음
-video_file = '.\\app\\test.mp4'  # 변환할 비디오 파일 경로
-text = video_to_text(video_file)
-print(text)
